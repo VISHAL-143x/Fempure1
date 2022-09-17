658b4faefa3d2191c0aedb71bbefd4848a0e6377
VISHAL PATIL <81164072+VishalPachpor@users.noreply.github.com>
2022-09-17 10:55:27 +0530

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,7 +1,7 @@
 export const products = [
   {
-    id: 'Sanitary Pads',
-    name: 'Sanitary Pads',
+    id: 'Cookies',
+    name: 'Cookies',
     Image:
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg80003327.weyesimg.com%2Fuploads%2Fwww.vdiaper.com%2Fimages%2F16075822007088.jpg%3FimageView2%2F2%2Fw%2F1920%2Fq%2F90&f=1&nofb=1',
     description: 'A Hyginic Product',
@@ -10,8 +10,8 @@ export const products = [
     priceUsd: 1,
   },
   {
-    id: 'Tampons',
-    name: 'Tampons',
+    id: 'Cakes',
+    name: 'Cup-cakes',
     Image:
       'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.summitmedia-digital.com%2Fcosmo%2Fimages%2Fnovember2014%2F11-19%2Ftampon-main-image.jpg&f=1&nofb=1',
     description: 'A Hyginic Product ',
@@ -20,12 +20,12 @@ export const products = [
     priceUsd: 1,
   },
   {
-    id: 'PantyLiners',
-    name: 'Panty Liners',
+    id: 'Chocolates',
+    name: 'Chocolates',
     Image:
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lkfIHfBEwRS9pW9TifdOswHaFj%26pid%3DApi&f=1',
     description: 'A Hyginic Product',
-    unitName: 'pack', // shows after the price, eg. 0.05 SOL/box
+    unitName: 'Box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.1,
     priceUsd: 1.5,
   },
